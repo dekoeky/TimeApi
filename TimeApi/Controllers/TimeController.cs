3c51b41a585ffde0cccc8b3d3646982049347d7d
Wesley Van Koeckhoven <wvk@outlook.be>
2023-07-16 13:22:22 +0200

    add editor config

--- TimeApi/Controllers/TimeController.cs
+++ TimeApi/Controllers/TimeController.cs
@@ -1,28 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 
 //TODO: File Scoped Namespace
-namespace TimeApi.Controllers
+namespace TimeApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimeController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class TimeController : ControllerBase
+    private readonly ILogger<TimeController> _logger;
+
+    public TimeController(ILogger<TimeController> logger)
     {
-        private readonly ILogger<TimeController> _logger;
-
-        public TimeController(ILogger<TimeController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("Local", Name = nameof(GetLocalTime))]
-        public DateTime GetLocalTime() => DateTime.Now;
-
-        [HttpGet("Utc", Name = nameof(GetUtcTime))]
-        public DateTime GetUtcTime() => DateTime.UtcNow;
-
-        //TODO: TimeZone Info
-        //TODO: DateTimeOffset
-        //TODO: DateOnly
-        //TODO: TimeOnly
+        _logger = logger;
     }
+
+    [HttpGet("Local", Name = nameof(GetLocalTime))]
+    public DateTime GetLocalTime() => DateTime.Now;
+
+    [HttpGet("Utc", Name = nameof(GetUtcTime))]
+    public DateTime GetUtcTime() => DateTime.UtcNow;
+
+    //TODO: TimeZone Info
+    //TODO: DateTimeOffset
+    //TODO: DateOnly
+    //TODO: TimeOnly
 }
